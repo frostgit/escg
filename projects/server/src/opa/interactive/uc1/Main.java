ed37e637ab6a8336c758f98e7a71f570cd5ef711
unknown <iced...@gmail.com>
2014-07-28 19:29:51 -0700

    + created project from eclipse

---
+++ projects/server/src/opa/interactive/uc1/Main.java
@@ -0,0 +1,28 @@
+package opa.interactive.uc1;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Robot robot = new Robot();
+			int mask = InputEvent.BUTTON1_DOWN_MASK;
+			int y = 0;
+			int x = 0;
+			robot.mouseMove(x, y);
+			robot.mousePress(mask);
+			robot.mouseRelease(mask);
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
